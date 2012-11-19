501e915193dc6aa59ff80ea194007e6010f7953c
Simon Elliott <simon@papercreatures.com>
2012-11-19 14:53:03 +0000

    gs

--- app/scripts/controllers/board.coffee
+++ app/scripts/controllers/board.coffee
@@ -1,3 +1,19 @@
+BoardModel = ($scope) ->
+  #width: 200
+  #height: 100
+  display: "block"
+  getCombined: ->
+    parseInt(@width, 10) + parseInt(@height, 10)
+
 goApp.controller "Board", ($scope) ->
-  $scope.awesomeThings = ["HTML5 Boilerplate", "AngularJS", "Testacular"]
-  $scope.rows = [0..8]
+  $scope.master = BoardModel
+  console.log $scope
+  $scope.pos = (style) -> style
+
+  $scope.rows = []
+  $scope.columns = []
+  max = 11
+  for i in [0 .. max]
+    pc = parseInt((100/max) * i,10)
+    $scope.rows.push top: "#{pc}%"
+    $scope.columns.push left: "#{pc}%"
--- app/scripts/controllers/board.js
+++ app/scripts/controllers/board.js
@@ -1,5 +1,33 @@
+var BoardModel;
+
+BoardModel = function($scope) {
+  return {
+    display: "block",
+    getCombined: function() {
+      return parseInt(this.width, 10) + parseInt(this.height, 10);
+    }
+  };
+};
 
 goApp.controller("Board", function($scope) {
-  $scope.awesomeThings = ["HTML5 Boilerplate", "AngularJS", "Testacular"];
-  return $scope.rows = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+  var i, max, pc, _i, _results;
+  $scope.master = BoardModel;
+  console.log($scope);
+  $scope.pos = function(style) {
+    return style;
+  };
+  $scope.rows = [];
+  $scope.columns = [];
+  max = 11;
+  _results = [];
+  for (i = _i = 0; 0 <= max ? _i <= max : _i >= max; i = 0 <= max ? ++_i : --_i) {
+    pc = parseInt((100 / max) * i, 10);
+    $scope.rows.push({
+      top: "" + pc + "%"
+    });
+    _results.push($scope.columns.push({
+      left: "" + pc + "%"
+    }));
+  }
+  return _results;
 });
