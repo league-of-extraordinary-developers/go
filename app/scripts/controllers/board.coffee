f6bb772d57344f8d2c3f5cfa575d74cfed7bb6df
Simon Elliott <simon@papercreatures.com>
2012-11-19 16:02:21 +0000

    go

--- app/scripts/controllers/board.coffee
+++ app/scripts/controllers/board.coffee
@@ -1,19 +1,36 @@
-BoardModel = ($scope) ->
-  #width: 200
-  #height: 100
-  display: "block"
-  getCombined: ->
-    parseInt(@width, 10) + parseInt(@height, 10)
+data =
+  go: 1
+  gridSize: 9
+  positions: []
+
+for x in [0..data.gridSize]
+  for y in [0..data.gridSize]
+    data.positions[x] ||= []
+    data.positions[x][y] =
+      owner: 0
+      top: "#{parseInt((100/(data.gridSize+2)) * x,10)}%"
+      left: "#{parseInt((100/(data.gridSize+2)) * y,10)}%"
+
 
 goApp.controller "Board", ($scope) ->
-  $scope.master = BoardModel
-  console.log $scope
+  $scope.pieces = data.positions
+
   $scope.pos = (style) -> style
+  $scope.className = (style) ->
+    switch style.owner
+      when 1 then "white"
+      when -1 then "black"
+      else ""
+
+  $scope.setPiece = (row) ->
+    if row.owner == 0
+      row.owner = data.go
+      data.go = -data.go
 
   $scope.rows = []
   $scope.columns = []
-  max = 11
-  for i in [0 .. max]
-    pc = parseInt((100/max) * i,10)
+
+  for i in [0 .. data.gridSize+2]
+    pc = parseInt((100/(data.gridSize+2)) * i,10)
     $scope.rows.push top: "#{pc}%"
     $scope.columns.push left: "#{pc}%"
--- app/scripts/controllers/board.js
+++ app/scripts/controllers/board.js
@@ -1,27 +1,49 @@
-var BoardModel;
+var data, x, y, _base, _i, _j, _ref, _ref1;
 
-BoardModel = function($scope) {
-  return {
-    display: "block",
-    getCombined: function() {
-      return parseInt(this.width, 10) + parseInt(this.height, 10);
-    }
-  };
+data = {
+  go: 1,
+  gridSize: 9,
+  positions: []
 };
 
+for (x = _i = 0, _ref = data.gridSize; 0 <= _ref ? _i <= _ref : _i >= _ref; x = 0 <= _ref ? ++_i : --_i) {
+  for (y = _j = 0, _ref1 = data.gridSize; 0 <= _ref1 ? _j <= _ref1 : _j >= _ref1; y = 0 <= _ref1 ? ++_j : --_j) {
+    (_base = data.positions)[x] || (_base[x] = []);
+    data.positions[x][y] = {
+      owner: 0,
+      top: "" + (parseInt((100 / (data.gridSize + 2)) * x, 10)) + "%",
+      left: "" + (parseInt((100 / (data.gridSize + 2)) * y, 10)) + "%"
+    };
+  }
+}
+
 goApp.controller("Board", function($scope) {
-  var i, max, pc, _i, _results;
-  $scope.master = BoardModel;
-  console.log($scope);
+  var i, pc, _k, _ref2, _results;
+  $scope.pieces = data.positions;
   $scope.pos = function(style) {
     return style;
   };
+  $scope.className = function(style) {
+    switch (style.owner) {
+      case 1:
+        return "white";
+      case -1:
+        return "black";
+      default:
+        return "";
+    }
+  };
+  $scope.setPiece = function(row) {
+    if (row.owner === 0) {
+      row.owner = data.go;
+      return data.go = -data.go;
+    }
+  };
   $scope.rows = [];
   $scope.columns = [];
-  max = 11;
   _results = [];
-  for (i = _i = 0; 0 <= max ? _i <= max : _i >= max; i = 0 <= max ? ++_i : --_i) {
-    pc = parseInt((100 / max) * i, 10);
+  for (i = _k = 0, _ref2 = data.gridSize + 2; 0 <= _ref2 ? _k <= _ref2 : _k >= _ref2; i = 0 <= _ref2 ? ++_k : --_k) {
+    pc = parseInt((100 / (data.gridSize + 2)) * i, 10);
     $scope.rows.push({
       top: "" + pc + "%"
     });
