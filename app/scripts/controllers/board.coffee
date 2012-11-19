4c30b8312f3f5bea1917e0b8137927bd1aa3d426
Simon Elliott <simon@papercreatures.com>
2012-11-19 12:37:30 +0000

    oops

---
+++ app/scripts/controllers/board.coffee
@@ -0,0 +1,3 @@
+goApp.controller "Board", ($scope) ->
+  $scope.awesomeThings = ["HTML5 Boilerplate", "AngularJS", "Testacular"]
+  $scope.rows = [0..8]
---
+++ app/scripts/controllers/board.js
@@ -0,0 +1,5 @@
+
+goApp.controller("Board", function($scope) {
+  $scope.awesomeThings = ["HTML5 Boilerplate", "AngularJS", "Testacular"];
+  return $scope.rows = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+});
