5a83fcd35f8abfb8f3f4f15b243f490c05022605
Simon Elliott <simon@papercreatures.com>
2012-11-19 10:30:50 +0000

    angular

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var goApp = angular.module('goApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
